006873bdd9e278789842924cdd45bd8d0a2b9444
JulesUSG15 <jules.ginhac@etu.univ-lyon1.fr>
2023-11-06 15:08:07 +0100

    classe OperandeLitteral

---
+++ TP2/OperandeLitteral.java
@@ -0,0 +1,35 @@
+package TP2;
+
+/**
+ * Classe représentant une opérande littérale
+ */
+public class OperandeLitteral extends Expression {
+    /**
+     * Valeur de l'opérande littérale
+     */
+    private ValeurTypee valeur;
+
+    /**
+     * Constructeur de la classe OperandeLitteral
+     * @param valeur
+     */
+    public OperandeLitteral(ValeurTypee valeur) {
+        this.valeur = valeur;
+
+    /**
+     * Renvoie la valeur de l'opérande
+     * @return la valeur de l'opérande
+     */
+    public ValeurTypee calculer () {
+        return this.valeur;
+    }
+
+    /**
+     * Renvoie une chaine de caractère representant la valeur de l'opérande
+     * @return une chaine de caractère representant la valeur de l'opérande
+     */
+    public String toString() {
+        return this.valeur.toString();
+    }
+}
+
