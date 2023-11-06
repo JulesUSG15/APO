e4fd7dfd300fb1cd1ee7bc55c7c8ed1525d689a5
JulesUSG15 <jules.ginhac@etu.univ-lyon1.fr>
2023-11-06 15:50:27 +0100

    Calsse Multiplication

---
+++ TP2/Multiplication.java
@@ -0,0 +1,40 @@
+package TP2;
+
+/**
+ * Classe représentant une multiplication entre deux expressions
+ */
+ public class Multiplication extends ExpressionBinaire {
+
+    /**
+     * Constructeur de la classe Multiplication
+     * @param expressionGauche
+     * @param expressionDroite
+     */
+    public Multiplication(Expression expressionGauche, Expression expressionDroite) {
+        super(expressionGauche, expressionDroite);
+    }
+
+    /**
+     * Renvoie la valeur de la multiplication des deux expressions
+     * @return la valeur de la multiplication des deux expressions
+     */
+    public ValeurTypee calculer() {
+        return this.getExpressionGauche().calculer().fois(this.getExpressionDroite().calculer());
+    }
+
+    /**
+     * Renvoie une chaine de caractère representant la multiplication des deux expressions
+     * @return une chaine de caractère representant la multiplication des deux expressions
+     */
+    public String toString() {
+        return "(" + this.getExpressionGauche().toString() + "*" + this.getExpressionDroite().toString() + ")";
+    }
+
+    /**
+     * Renvoie l'opérateur de la multiplication
+     * @return l'opérateur de la multiplication
+     */
+    public String getOperateur() {
+        return "*";
+    }
+ }
